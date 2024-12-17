f29adf160791ee81571376f09d4226edde172f01
wannurz <6560506007@pnu.ac.th>
2024-12-17 15:38:36 +0700

    Create bits.ts

---
+++ bits.ts
@@ -0,0 +1,7 @@
+//% weight=100 color=#0fbc11 icon="\uf10c"
+namespace Parkwan {
+    //% blockId="id_pow" block="%base | raised to %exp"
+    export function fn_raiseto(base: number, exp: number): number {
+        return Math.pow(base, exp);
+    }
+}
